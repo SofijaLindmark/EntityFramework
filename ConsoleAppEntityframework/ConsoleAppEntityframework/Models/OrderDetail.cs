d3fa8e5d65def19709f9c397ad582276c612ee8f
SofijaLindmark <sofija_andersdotter@hotmail.com>
2024-05-04 19:36:21 +0200

    Add files via upload

---
+++ ConsoleAppEntityframework/ConsoleAppEntityframework/Models/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntityframework.Models
+{
+   public class OrderDetail
+    {
+        public int Id { get; set; }
+        public string Quantity { get; set; }
+        public string ProductId { get; set; }
+
+        public string OrderId { get; set; }
+
+        public Orders Order { get; set; } = null!;
+
+        public Products Product { get; set; } = null!;
+    }
+}
---
+++ ConsoleAppEntityframework/ConsoleAppEntityframework/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntityframework.Models
+{
+    public class Products
+    {
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        [Column(TypeName ="decimal(6, 2)")]
+
+        public decimal Price { get; set; }
+    }
+}
